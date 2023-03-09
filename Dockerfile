fafc745c8f7578ed854690f4e190507789978159
mayhemlikemee <61032130+mayhemlikemee@users.noreply.github.com>
2023-03-09 15:37:38 -0600

    We got it working so far

--- Dockerfile
+++ Dockerfile
@@ -11,23 +11,25 @@ EXPOSE 8000
 
 
 ARG DEV=false
-RUN python -m venv /py && \
-    /py/bin/pip install --upgrade pip && \
-    apk add --update --no-cache postgresql-client && \
-    apk add --update --no-cache --virtual .tmp-build-deps \
-        build-base postgresql-dev musl-dev && \
-    /py/bin/pip install -r /tmp/requirements.txt && \
-    if [ $DEV = "true" ]; \
+
+RUN python -m venv /py
+RUN /py/bin/pip install --upgrade pip
+RUN sed -i 's/https/http/' /etc/apk/repositories
+# RUN source
+RUN apk add --update --no-cache postgresql-client
+RUN apk add --update --no-cache --virtual .tmp-build-deps \
+        build-base postgresql-dev musl-dev
+RUN /py/bin/pip install -r /tmp/requirements.txt
+RUN if [ $DEV = "true" ]; \
         then /py/bin/pip install -r /tmp/requirements.dev.txt ; \
-    fi && \
-    rm -rf /tmp && \
+    fi
+RUN rm -rf /tmp && \
     apk del .tmp-build-deps && \
     adduser \
         --disabled-password \
         --no-create-home \
-        django-user
+        django-user 
 
 ENV PATH="/py/bin:$PATH"
 
-USER django-user
-
+USER root
--- app/app/settings.py
+++ app/app/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x3pzg-1n+y2qbt8%!!k(z-!(b^gdk17ho6^-=pir)k7)$af7hk'
+SECRET_KEY = 'django-insecure-+^mx4(*3&c!6_mn$#mnn7$6mb2mdpkm3d=u=pc2hs8*2_-bx!u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ['DB_HOST'],
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASS']
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
